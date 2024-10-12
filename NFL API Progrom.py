b608df33fa9874259ca615c497ba0f3fd9415597
mason <mason.teal21@gmail.com>
2024-10-12 18:16:48 -0400

    Basic Menu, put filler stuff in to be filled out as I work on this. Started with the first menu option and function so I can work on Live Game Stats function next

--- NFL API Progrom.py
+++ NFL API Progrom.py
@@ -1,5 +1,48 @@
+from time import sleep
+
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 api_key = os.getenv('API_KEY')
+
+def menu():
+    while True:
+        print("Welcome")
+
+        sleep(2.5)
+
+        for i in range(1, 30):
+            print("")
+
+        print("This Program will allow you to harness the power of technology to see various stats about the NFL. Includes:")
+        print("*Live Stat Coverage")
+        print("*Filler")
+        print("*Filler")
+        print("*Filler")
+        print("*Filler")
+        print("*Filler")
+        print("*Filler")
+        print("*And more soon to come!")
+        sleep(8)
+
+        for i in range(1, 30):
+            print("")
+
+        print("Choose from the available options:")
+        print("1. Live Game Stats")
+        print("?. Filler")
+        print("?. Filler")
+        print("?. Filler")
+        print("?. Filler")
+        print("2. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            view_live_game_stat()
+        elif choice == "2":
+            exit()
+menu()
+
+def view_live_game_stat():
+    return
